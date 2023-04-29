306bcd01199f633e07b409ebb14e80151547ba79
barry-kn <carrybarryklein49@gmail.com>
2023-04-29 14:52:46 +0300

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
+import "./App.css";
 
 function App() {
+  const [bots, setBots] = useState([]);
+  const [enlistedBots, setEnlistedBots] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8001/bots")
+      .then((response) => response.json())
+      .then((data) => setBots(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const enlistBot = (id) => {
+    const botToAdd = bots.find((bot) => bot.id === id);
+    setEnlistedBots([...enlistedBots, botToAdd]);
+    setBots(
+      bots.map((bot) => {
+        if (bot.id === id) {
+          return {
+            ...bot,
+            enlisted: true,
+          };
+        } else {
+          return bot;
+        }
+      })
+    );
+  };
+
+  const releaseBot = (id) => {
+    setEnlistedBots(enlistedBots.filter((bot) => bot.id !== id));
+    setBots(
+      bots.map((bot) => {
+        if (bot.id === id) {
+          return {
+            ...bot,
+            enlisted: false,
+          };
+        } else {
+          return bot;
+        }
+      })
+    );
+  };
+
+  const dischargeBot = (id) => {
+    setEnlistedBots(enlistedBots.filter((bot) => bot.id !== id));
+    fetch(`http://localhost:8001/bots/${id}`, { method: "DELETE" })
+      .then((response) => response.json())
+      .then((data) => {
+        setBots(bots.filter((bot) => bot.id !== id));
+      })
+      .catch((error) => console.error(error));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Bot Battlr</h1>
       </header>
+      <main>
+        <BotCollection bots={bots} enlistBot={enlistBot} />
+        <YourBotArmy
+          enlistedBots={enlistedBots}
+          releaseBot={releaseBot}
+          dischargeBot={dischargeBot}
+        />
+      </main>
     </div>
   );
 }
